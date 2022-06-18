aa01d003f27c6ba73748b08b208ebafd7e0968ee
homin <ghals5737@naver.com>
2022-06-18 18:50:51 +0900

    [feat]: 테스트 코드 추가,예약 조회 에러 코드 추가
    1.예약 번호로 삭제 API 추가
    2.예약 저장 테스트 코드 추가
    3.해당 예약정보가 없는 경우 에로코드 추가
    4.예약 API응답 ApiResponse로 수정
    5.ApiResponse createEror 코드 수정

--- restApiServer/src/main/java/com/esctb/restapiserver/domain/reservation/controller/ReservationController.java
+++ restApiServer/src/main/java/com/esctb/restapiserver/domain/reservation/controller/ReservationController.java
@@ -3,6 +3,8 @@ package com.esctb.restapiserver.domain.reservation.controller;
 import com.esctb.restapiserver.domain.reservation.dto.ReservationDto;
 import com.esctb.restapiserver.domain.reservation.entity.Reservation;
 import com.esctb.restapiserver.domain.reservation.service.ReservationService;
+import com.esctb.restapiserver.global.common.ApiResponse;
+import com.esctb.restapiserver.global.error.exception.CustomException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,19 +17,40 @@ public class ReservationController {
 
     private final ReservationService reservationService;
 
-    @GetMapping()
-    public ReservationDto.ReservationResponse getReservation(Long id){
-        return reservationService.getReservation(id);
+    @GetMapping("/{id}")
+    public ApiResponse getReservation(@PathVariable Long id){
+        try{
+            return ApiResponse.createSuccess(reservationService.getReservation(id));
+        }catch (CustomException e){
+            return ApiResponse.createError(e.getErrorCode());
+        }
     }
 
     @GetMapping("products/{product_id}")
-    public ReservationDto.ReservationResponse getReservationByProductId(@PathVariable Long product_id){
-        return reservationService.getReservationByProductId(product_id);
+    public ApiResponse getReservationByProductId(@PathVariable Long product_id){
+        try{
+            return ApiResponse.createSuccess(reservationService.getReservationByProductId(product_id));
+        }catch (CustomException e){
+            return ApiResponse.createError(e.getErrorCode());
+        }
     }
 
     @PostMapping()
-    public ReservationDto.ReservationResponse saveReservation(@RequestBody ReservationDto.ReservationCreateRequest request){
-        return reservationService.saveReservation(request);
+    public ApiResponse saveReservation(@RequestBody ReservationDto.ReservationCreateRequest request){
+        try{
+            return ApiResponse.createSuccess(reservationService.saveReservation(request));
+        }catch (CustomException e){
+            return ApiResponse.createError(e.getErrorCode());
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ApiResponse deleteReservation(@PathVariable Long id){
+        try{
+            return ApiResponse.createSuccess(reservationService.deleteReservation(id));
+        }catch (CustomException e){
+            return ApiResponse.createError(e.getErrorCode());
+        }
     }
 
 }
--- restApiServer/src/main/java/com/esctb/restapiserver/domain/reservation/service/ReservationServiceImpl.java
+++ restApiServer/src/main/java/com/esctb/restapiserver/domain/reservation/service/ReservationServiceImpl.java
@@ -3,6 +3,8 @@ package com.esctb.restapiserver.domain.reservation.service;
 import com.esctb.restapiserver.domain.reservation.dto.ReservationDto;
 import com.esctb.restapiserver.domain.reservation.entity.Reservation;
 import com.esctb.restapiserver.domain.reservation.repository.ReservationRepository;
+import com.esctb.restapiserver.global.error.ErrorCode;
+import com.esctb.restapiserver.global.error.exception.CustomException;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,18 +16,17 @@ import java.util.Optional;
 @Service
 public class ReservationServiceImpl implements ReservationService{
 
-    @Autowired
     private final ReservationRepository reservationRepository;
 
     @Override
     public ReservationDto.ReservationResponse getReservation(Long id) {
-        return ReservationDto.ReservationResponse.builder().build().toReservation(reservationRepository.findById(id).orElse(null));
+        return ReservationDto.ReservationResponse.builder().build().toReservation(reservationRepository.findById(id).orElseThrow(()->new CustomException(ErrorCode.RESERVATION_NOT_FOUND)));
     }
 
     @Override
     public ReservationDto.ReservationResponse getReservationByProductId(Long productId) {
         return ReservationDto.ReservationResponse.builder().build().toReservation(
-                reservationRepository.findByProductId(productId).orElse(null)
+                reservationRepository.findByProductId(productId).orElseThrow(()->new CustomException(ErrorCode.RESERVATION_NOT_FOUND))
         );
     }
 
@@ -34,5 +35,12 @@ public class ReservationServiceImpl implements ReservationService{
         return ReservationDto.ReservationResponse.builder().build().toReservation(
                 reservationRepository.save(request.toReservation())
         );
+    }
+
+    @Override
+    public ReservationDto.ReservationResponse deleteReservation(Long id) {
+        Reservation reservation=reservationRepository.findById(id).orElseThrow(()->new CustomException(ErrorCode.RESERVATION_NOT_FOUND));
+        reservationRepository.delete(reservation);
+        return ReservationDto.ReservationResponse.builder().build().toReservation(reservation);
     }
 }
