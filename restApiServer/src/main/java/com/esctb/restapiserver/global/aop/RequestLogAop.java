e10f873e1ab6ca72645b8f399f4a6b88eac5bece
homin <ghals5737@naver.com>
2022-06-20 21:13:34 +0900

    충돌해소 커밋

--- restApiServer/src/main/java/com/esctb/restapiserver/global/aop/RequestLogAop.java
+++ restApiServer/src/main/java/com/esctb/restapiserver/global/aop/RequestLogAop.java
@@ -1,5 +1,7 @@
 package com.esctb.restapiserver.global.aop;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
@@ -8,13 +10,18 @@ import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.util.ContentCachingRequestWrapper;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
 
 @Aspect
 @Component
+@RequiredArgsConstructor
 @Slf4j
 public class RequestLogAop {
+
+    private final ObjectMapper objectMapper;
 
     @Pointcut("execution(* com.esctb.restapiserver.domain.*.controller.*.*(..))")
     public void cut(){}
