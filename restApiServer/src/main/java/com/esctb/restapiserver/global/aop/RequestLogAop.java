46c2704df39ed6fb908f881c378df76f5d8594cc
homin <ghals5737@naver.com>
2022-06-20 22:32:48 +0900

    [feat]: 에러 로그 수정
    1. request aop 로그 수정
    2. controller advice 로깅 수정
    3. slack 메시지를 보내기 위한 SlackUtils 추가
    4. controller advice에서 CustomException 발생시 슬랙으로 로그 보냄

--- restApiServer/src/main/java/com/esctb/restapiserver/global/aop/RequestLogAop.java
+++ restApiServer/src/main/java/com/esctb/restapiserver/global/aop/RequestLogAop.java
@@ -6,13 +6,18 @@ import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 @Aspect
@@ -32,6 +37,8 @@ public class RequestLogAop {
                 .currentRequestAttributes())
                 .getRequest();
 
+        final ContentCachingRequestWrapper cachingRequestWrapper=(ContentCachingRequestWrapper) request;
+
         Object value;
 
         try{
@@ -39,11 +46,13 @@ public class RequestLogAop {
         }catch (Throwable throwable){
             throw throwable;
         }finally {
-            log.info(
-                    "/ᐠ｡ꞈ｡ᐟ✿\\ {} {} from {} /ᐠ｡‸｡ᐟ\\",
-                    request.getMethod(),
-                    request.getRequestURI(),
-                    request.getRemoteAddr());
+            log.info("===============================API Reqeust===============================");
+            log.info("Request Time: {}", LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy:MM:dd HH:mm:ss")));
+            log.info("Method: {}",request.getMethod());
+            log.info("URL: {}",request.getRequestURL());
+            log.info("URI: {}",request.getRequestURI());
+            log.info("QueryString: {}",request.getQueryString()==null?"":request.getQueryString());
+            log.info("Request Body: {}",objectMapper.readTree(cachingRequestWrapper.getContentAsByteArray()));
         }
         return value;
     }
