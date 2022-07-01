bdb18d5fc79c1b30b3140c47dbcc5200d46d5104
yeon <yooyeon.dev@gmail.com>
2022-07-01 20:19:49 +0900

    [feat]: 모든 상품 조회 querydsl 로 변경, ProductImage 저장 시 연관관계 고려

--- restApiServer/src/main/java/com/esctb/restapiserver/domain/product/repository/ProductRepositoryCustomImpl.java
+++ restApiServer/src/main/java/com/esctb/restapiserver/domain/product/repository/ProductRepositoryCustomImpl.java
@@ -2,14 +2,16 @@ package com.esctb.restapiserver.domain.product.repository;
 
 import com.esctb.restapiserver.domain.product.entity.Product;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import java.util.List;
 
-import static com.esctb.restapiserver.domain.product.entity.QCategory.category;
+import static com.esctb.restapiserver.domain.category.entity.QCategory.category;
 import static com.esctb.restapiserver.domain.product.entity.QProduct.product;
-
+import static com.esctb.restapiserver.domain.product.entity.QProductImage.productImage;
+@Slf4j
 @Repository
 public class ProductRepositoryCustomImpl implements ProductRepositoryCustom {
     private final JPAQueryFactory queryFactory;
@@ -23,7 +25,17 @@ public class ProductRepositoryCustomImpl implements ProductRepositoryCustom {
         return queryFactory
                 .select(product)
                 .from(product)
-                .leftJoin(product.category,category)
+                .leftJoin(product.category, category)
                 .fetch();
+    }
+
+    @Override
+    public List<Product> findAllProducts() {
+        List<Product> fetch = queryFactory
+                .selectFrom(product)
+                .join(product.productImages, productImage)
+                .fetch();
+        log.info("fetch={}",fetch);
+        return fetch;
     }
 }
